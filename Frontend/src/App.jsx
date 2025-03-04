0c74227eeaa793aa49d782752281a49dcd734012
yoni2z <zyonas626@gmail.com>
2025-03-04 11:55:21 +0300

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-8xl font-bold text-green-500 text-center mt-10'>Welcome! This is Website for Great Designs.</h1>
+    </>
+  )
+}
+
+export default App
